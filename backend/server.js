a4b526b8e2201ecd7feb7184994cbe444f1b1999
Martyvole <190987379+Martyvole@users.noreply.github.com>
2024-12-26 11:33:30 +0100

    Initial backend setup.

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Testovací endpoint
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
